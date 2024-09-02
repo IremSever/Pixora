1b9a097cce57d6bc4408bc1e9b87ff628c08b23e
İrem Sever <iremsever_@outlook.com>
2024-09-02 15:22:26 +0300

    created

---
+++ Pixora/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pixora
+//
+//  Created by İrem Sever on 2.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
